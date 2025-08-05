ab6e3c5423b78008566f160704244237d678df4d
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-08-04 19:11:32 -0600

    TablaTarea

---
+++ src/app/Components/TablaTarea.tsx
@@ -0,0 +1,37 @@
+import {useContextGestor} from "@/app/Provider/GestorProvider";
+
+export const TablaTarea = () => {
+
+    const {tarea}=useContextGestor()
+
+
+    return (
+        <>
+            <h2>Lista de Tarea</h2>
+
+            <table border={1}>
+                <thead>
+                <th>id Tarea</th>
+                <th>Titulo</th>
+                <th>Descripcion</th>
+                <th>Estado</th>
+                </thead>
+                <tbody>
+                {
+                    tarea.map((tarea)=>(
+                        <tr key={tarea.id}>
+                            <td>{tarea.id}</td>
+                            <td>{tarea.titulo}</td>
+                            <td>{tarea.descripcion}</td>
+                            <td>{tarea.estado}</td>
+
+                        </tr>
+
+                    ))
+                }
+                </tbody>
+            </table>
+        </>
+    )
+
+}
